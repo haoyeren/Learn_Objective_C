b5f595e30463dfc566e2e2347f982321156d0a82
John Zhou <haoyeren@hotmail.com>
2015-09-04 19:23:04 +0800

    first program, hello objective-c

---
+++ 001_Hello_Objective-C/Hello Objective-C/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hello Objective-C
+//
+//  Created by 周衡 on 15/9/4.
+//  Copyright (c) 2015年 周衡. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    
+    NSLog(@"Hello, World!");
+    
+    return 0;
+}
